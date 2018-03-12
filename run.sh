295abeb21e93df85d42a17b6ca07476a49b85274
Adam Eivy <Adam.Eivy@disney.com>
2018-03-12 16:39:16 -0700

    adding a helper script to allow me to spin up more localhost environments to share with students if other means fail :)

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# This is a helper script for the teachers to spin up localhost instances of a docker container 
+# to share over the network with students who are having trouble running the project themselves
+
+port=${1:-8888}
+ip=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1' | head -n 1)
+echo "creating project on http://$ip:$port"
+cp -R . ../ml_class_$port
+cd ../ml_class_$port
+docker run -it -v $(pwd):/home/jovyan --rm -p $port:8888 jupyter/scipy-notebook
