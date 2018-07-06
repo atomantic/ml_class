4d12cddfebef8a9e18a79a9d4d5a21980a3e584f
Adam Eivy <Adam.Eivy@disney.com>
2018-07-06 11:50:49 -0700

    fix run script (exclude .git) and add simple talking points of what ML is

--- run.sh
+++ run.sh
@@ -6,6 +6,7 @@
 port=${1:-8888}
 ip=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1' | head -n 1)
 echo "creating project on http://$ip:$port"
-cp -R . ../ml_class_$port
+mkdir -p ../ml_class_$port
+tar cf - --exclude=.git . | (cd ../ml_class_$port && tar xvf - )
 cd ../ml_class_$port
 docker run -it -v $(pwd):/home/jovyan --rm -p $port:8888 jupyter/scipy-notebook
